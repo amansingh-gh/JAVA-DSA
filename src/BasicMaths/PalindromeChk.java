d463fccf47a0549e0cf938820f90df6c1446a9f5
AMN-TUF <yo.akaman@gmail.com>
2026-01-05 23:59:18 +0530

    Updating codes

--- src/BasicMaths/PalindromeChk.java
+++ src/BasicMaths/PalindromeChk.java
@@ -1,5 +1,31 @@
 package BasicMaths;
 
+import java.util.Scanner;
+
 public class PalindromeChk {
-    
+    static Boolean chkPalindrome(int n){
+        int dupli = n;
+        int revNum = 0;
+        while(n>0){
+            int rem = n%10;
+            revNum = revNum*10+rem;
+            n = n/10;
+        }
+        if(dupli!=revNum){
+            return false;
+        }
+        return true;
+    }
+
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Boolean res = chkPalindrome(n);
+        if (res) {
+            System.out.println("Yes");
+        }else  {
+            System.out.println("No");
+        }
+
+    }
 }
